c5aa532891b0917330123601d5b0284a5c243f34
nahrens89 <n.ahrens@dpv-analytics.com>
2025-07-21 17:19:30 +0200

    feat: added new linter for markdown

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -16,18 +16,19 @@ return {
 				'ruff',
 				'prettier',
 				'shfmt',
+				'vale',
 			},
 			automatic_installion = true,
 		
 			}
 			local null_ls = require 'null-ls'
 			local sources = {
-				require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I'}},
-				require'none-ls.formatting.ruff_format',
+				require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I', '--line-length=120',}},
+				require('none-ls.formatting.ruff_format').with { extra_args = {  '--line-length=120',
+  }},
 				null_ls.builtins.formatting.prettier.with { filetypes = { 'json', 'yaml', 'markdown'}},
 				null_ls.builtins.formatting.shfmt.with { args = { '-i', '4', 'markdown'}},
 			}
-			
 			local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 			null_ls.setup {
 				--debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
@@ -41,14 +42,11 @@ return {
 							buffer = bufnr,
 							callback = function ()
 								vim.lsp.buf.format { async = false }
-								
 							end,
 						})
 				end
 				end,
-			} 
+			}
 		end,
-		
-
 	},
 }
