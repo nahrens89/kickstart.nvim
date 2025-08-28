ca226f2bc3ab814f96aaf7c748aebed9ff3a81fe
nahrens89 <n.ahrens@dpv-analytics.com>
2025-08-28 10:50:27 +0200

    feat: removed null-ls instead using conform

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,55 +3,58 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {
-    'nvimtools/none-ls.nvim',
-    dependencies = {
-      'nvimtools/none-ls-extras.nvim',
-      'jayp0521/mason-null-ls.nvim',
-    },
-    config = function()
-      -- list of formatters & linters for mason to install
-      require('mason-null-ls').setup {
-        ensure_installed = {
-          'ruff',
-          'prettier',
-          'shfmt',
-          'stylua',
-          'vale',
-        },
-        automatic_installation = true,
-      }
-      local null_ls = require 'null-ls'
-      local sources = {
-        require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I', '--line-length=120' } },
-        require('none-ls.formatting.ruff_format').with { extra_args = { '--line-length=120' } },
-        null_ls.builtins.formatting.prettier.with {
-          filetypes = { 'json', 'yaml', 'markdown','html', 'css', 'javascript', 'typescript' },
-          extra_args = { '--print-width', '120', '--prose-wrap', 'always','--tab-width', '2', '--indent-type', 'Spaces', '--indent-width', '2'}
-        },
-        null_ls.builtins.formatting.shfmt.with { extra_args = { '--indent-type', 'Spaces', '--indent-width', '4' } },
-        null_ls.builtins.formatting.stylua.with { extra_args = { '--indent-type', 'Spaces', '--indent-width', '2' } },
-      }
-      local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-      null_ls.setup {
-        debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
-        sources = sources,
-        -- you can reuse a shared lspconfig on_attach callback here
-        on_attach = function(client, bufnr)
-          if client.supports_method 'textDocument/formatting' then
-            vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-            vim.api.nvim_create_autocmd('BufWritePre', {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format { async = false }
-              end,
-            })
-          end
-        end,
-      }
-    end,
-  },
+  -- Disabled none-ls in favor of nvim-lint for Python linting
+  -- {
+  --   'nvimtools/none-ls.nvim',
+  --   dependencies = {
+  --     'nvimtools/none-ls-extras.nvim',
+  --     'jayp0521/mason-null-ls.nvim',
+  --   },
+  --   config = function()
+  --     -- list of formatters & linters for mason to install
+  --     require('mason-null-ls').setup {
+  --       ensure_installed = {
+  --         'ruff',
+  --         'prettier',
+  --         'shfmt',
+  --         'stylua',
+  --         'vale',
+  --       },
+  --       automatic_installation = true,
+  --     }
+  --     local null_ls = require 'null-ls'
+  --     local sources = {
+  --       -- Remove Ruff sources since we're using Ruff LSP server now
+  --       -- require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I', '--line-length=120' } },
+  --       -- require('none-ls.formatting.ruff_format').with { extra_args = { '--line-length=120' } },
+  --       null_ls.builtins.formatting.prettier.with {
+  --         filetypes = { 'json', 'yaml', 'markdown','html', 'css', 'javascript', 'typescript' },
+  --         extra_args = { '--print-width', '120', '--prose-wrap', 'always','--tab-width', '2', '--indent-type', 'Spaces', '--indent-width', '2'}
+  --       },
+  --       null_ls.builtins.formatting.shfmt.with { extra_args = { '--indent-type', 'Spaces', '--indent-width', '4' } },
+  --       null_ls.builtins.formatting.stylua.with { extra_args = { '--indent-type', 'Spaces', '--indent-width', '2' } },
+  --       null_ls.builtins.completion.spell,
+  --     }
+  --     local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+  --     null_ls.setup {
+  --       debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
+  --       sources = sources,
+  --       -- you can reuse a shared lspconfig on_attach callback here
+  --       on_attach = function(client, bufnr)
+  --         if client.supports_method 'textDocument/formatting' then
+  --           vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+  --           vim.api.nvim_create_autocmd('BufWritePre', {
+  --             group = augroup,
+  --             buffer = bufnr,
+  --             callback = function()
+  --               vim.lsp.buf.format { async = false }
+  --             end,
+  --           })
+  --         end
+  --       end,
+  --     }
+  --   end,
+  -- },
   {
     'linux-cultist/venv-selector.nvim',
     dependencies = {
