124ecbcb0605d73e413d1f0bafb7c003e20fb7dd
nahrens89 <n.ahrens@dpv-analytics.com>
2025-09-15 08:29:34 +0200

    feat: added markown and toml linte

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -31,10 +31,9 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        python = {
-          'ruff_format',
-          'ruff_organize_imports',
-        },
+        python = { 'ruff_format', 'ruff_organize_imports' },
+        toml = { 'tombi' },
+        markdown = { 'prettier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -238,7 +238,11 @@ return {
             },
           },
         },
-
+        markdownlint = {},
+        -- linter for javascript and markdown fileswe
+        eslint = {},
+        -- linter for toml files
+        tombi = {},
         ruff = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
